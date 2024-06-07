abdd6fdc6755ffaa1d34529f42caffd5e189e372
vinirmn007 <alexis.roman@unl.edu.ec>
2024-06-07 13:43:17 -0500

    arreglo de datail

--- Sensores/views.py
+++ Sensores/views.py
@@ -14,7 +14,24 @@ def sensor(request):
     return render(request, 'sensores.html', content)
 
 def sensorDetail(request, id):
-    sensor = Sensor.objects.get(id=id)
+    sensorTemp = SensorTemperatura.objects.all()
+    sensorPeso = SensorPeso.objects.all()
+    sensorGPS = SensorGPS.objects.all()
+    sensorProx = SensorProximidad.objects.all()
+    sensores_list = [sensorTemp, sensorPeso, sensorGPS, sensorProx]
+
+    for sensores in sensores_list:
+        for sensor in sensores:
+            if sensor.id == id:
+                if type(sensor) == SensorTemperatura:
+                    sensor = SensorTemperatura.objects.get(id=id)
+                elif type(sensor) == SensorPeso:
+                    sensor = SensorPeso.objects.get(id=id)
+                elif type(sensor) == SensorGPS:
+                    sensor = SensorGPS.objects.get(id=id)
+                elif type(sensor) == SensorProximidad:
+                    sensor = SensorProximidad.objects.get(id=id)
+
     content = {
         'sensor': sensor
     }
