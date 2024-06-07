fbac7e82612d036822a378b3defed3948fba03e1
Alexis Roman <166523266+vinirmn007@users.noreply.github.com>
2024-06-07 15:23:38 -0500

    Update settings.py

--- RobotPis/settings.py
+++ RobotPis/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
